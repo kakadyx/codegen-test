05ef70a6d476bb2702d73e89e16de5b9a483dc9a
Konstantin Krutskih <krutskih@artw.ru>
2023-12-23 23:36:01 +0300

    попробовал кодогенерацию, всё хорошо работает. Типы подтягиваются, апишка создаётся

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
+
+import {PetApi} from "../../generated/api";
+
+const petApi = new PetApi()
+
+console.log((await petApi.getPetById(10)).data.)
 </script>
 
 <template>
